4067c023c6c65846873f16990da1076dbf96344f
omeatai <omeatai@gmail.com>
2024-01-20 17:45:12 -0700

    2.Setup sample product
    2.Setup sample product

--- app/page.js
+++ app/page.js
@@ -1,3 +1,29 @@
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div className="p-6">
+      <div>
+        <h2 className="text-2xl font-medium text-gray-600">Mobiles</h2>
+        <div className="py-4">
+          <div className="w-64">
+            <div className="bg-purple-100 p-5 rounded-xl">
+              <img src="/products/iphone.png" alt="iphone" />
+            </div>
+            <div className="mt-2">
+              <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
+            </div>
+            <p className="mt-2 text-sm leading-4 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+              consequuntur doloreore!
+            </p>
+            <div className="mt-2 flex items-center">
+              <div className="text-2xl font-bold grow">$1200</div>
+              <button className="bg-violet-400 text-white py-1 px-3 rounded-xl hover:bg-violet-500 hover:scale-125">
+                +add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
