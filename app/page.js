76665b1af27519c956779c1f85e507205807ba06
omeatai <omeatai@gmail.com>
2024-01-21 18:20:48 -0700

    5. Showing Products from db with Products component
    5. Showing Products from db with Products component

--- app/page.js
+++ app/page.js
@@ -1,28 +1,77 @@
+"use client";
+import { useEffect, useState } from "react";
+import Product from "../components/Product";
+
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("/api/products", {
+          headers: {
+            Accept: "application/json",
+            method: "GET",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+
+        if (response) {
+          const data = await response.json();
+          // console.log(data);
+          setProducts(data);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchDataFromApi();
+  }, []);
+
+  const categoryList = products.map((product) => {
+    return product.category;
+  });
+  const uniqueCategories = [...new Set(categoryList)];
+
+  // console.log(uniqueCategories);
+
   return (
     <div className="p-6">
       <div>
-        <h2 className="text-2xl font-medium text-gray-600">Mobiles</h2>
-        <div className="py-4">
-          <div className="w-64">
-            <div className="bg-purple-100 p-5 rounded-xl">
-              <img src="/products/iphone.png" alt="iphone" />
-            </div>
-            <div className="mt-2">
-              <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
-            </div>
-            <p className="mt-2 text-sm leading-4 text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              consequuntur doloreore!
-            </p>
-            <div className="mt-2 flex items-center">
-              <div className="text-2xl font-bold grow">$1200</div>
-              <button className="bg-violet-400 text-white py-1 px-3 rounded-xl hover:bg-violet-500 hover:scale-125">
-                +add to cart
-              </button>
-            </div>
+        {loading && (
+          <div className="flex justify-center">
+            <div className="animate-spin rounded-full h-32 w-32 border-b-8 border-purple-500"></div>
           </div>
-        </div>
+        )}
+      </div>
+      <div>
+        {uniqueCategories.map((category) => {
+          return (
+            <div key={category} className="mt-8 ">
+              <h2 className="text-2xl font-medium text-gray-600 capitalize text-start">
+                {category}
+              </h2>
+              <div className="flex gap-4 overflow-x-scroll scrollbar-hide snap-x snap-start">
+                {products
+                  .filter((product) => product.category === category)
+                  .map((product) => {
+                    return (
+                      <div key={product.id} className="mt-4">
+                        <Product product={product} />
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ components/Product/Product.jsx
@@ -0,0 +1,51 @@
+export default function Product({ product }) {
+  const { name, price, picture, description, category } = product;
+  const bg_color = {
+    mobiles: "bg-purple-100 hover:bg-purple-200",
+    audio: "bg-blue-100 hover:bg-blue-200",
+    laptops: "bg-slate-100 hover:bg-slate-200",
+  };
+  return (
+    <div className="py-4">
+      <div className="w-64">
+        <div className={`${bg_color[category]} p-5 rounded-xl cursor-pointer`}>
+          <img src={picture} alt="iphone" />
+        </div>
+        <div className="mt-2">
+          <h3 className="font-bold text-lg">{name}</h3>
+        </div>
+        <p className="mt-2 text-sm leading-4 text-gray-500">{description}</p>
+        <div className="mt-2 flex items-center">
+          <div className="text-2xl font-bold grow">${price}</div>
+          <button className="bg-violet-400 text-white py-1 px-3 rounded-xl hover:bg-violet-500 hover:scale-125">
+            +add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// <div>
+//   <h2 className="text-2xl font-medium text-gray-600">Mobiles</h2>
+//   <div className="py-4">
+//     <div className="w-64">
+//       <div className="bg-purple-100 p-5 rounded-xl">
+//         <img src="/products/iphone.png" alt="iphone" />
+//       </div>
+//       <div className="mt-2">
+//         <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
+//       </div>
+//       <p className="mt-2 text-sm leading-4 text-gray-500">
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+//         consequuntur doloreore!
+//       </p>
+//       <div className="mt-2 flex items-center">
+//         <div className="text-2xl font-bold grow">$1200</div>
+//         <button className="bg-violet-400 text-white py-1 px-3 rounded-xl hover:bg-violet-500 hover:scale-125">
+//           +add to cart
+//         </button>
+//       </div>
+//     </div>
+//   </div>
+// </div>;
